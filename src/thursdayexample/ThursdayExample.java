d1fb68724d47b21f8918c9e2581e1603096f12f9
bonsujo <55116162+bonsujo@users.noreply.github.com>
2020-01-23 09:51:18 -0500

    Update ThursdayExample.java

--- src/thursdayexample/ThursdayExample.java
+++ src/thursdayexample/ThursdayExample.java
@@ -9,6 +9,7 @@ public class ThursdayExample {
 
     public static void main(String[] args) {
         System.out.println("Learning Github");
+        System.out.println("learning pull operations");
     }
     
 }
