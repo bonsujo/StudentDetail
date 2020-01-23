90d1862814bdbb2757838584498c5c8f2d7f95ce
bonsujo <55116162+bonsujo@users.noreply.github.com>
2020-01-23 10:08:47 -0500

    Update ThursdayExample.java

--- src/thursdayexample/ThursdayExample.java
+++ src/thursdayexample/ThursdayExample.java
@@ -1,4 +1,4 @@
-/* date january 23 2020
+/* date january 23 2020 :)
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,6 +10,7 @@ public class ThursdayExample {
     public static void main(String[] args) {
         System.out.println("Learning Github");
         System.out.println("learning pull operations");
+        
         
     }
     
